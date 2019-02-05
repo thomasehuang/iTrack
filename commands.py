0852f9e40fc3ab05b3b98705e49ab8e736e7bd9f
Thomas Huang <thomaseh@umich.edu>
2019-02-05 12:43:27 -0500

    Added commands.

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+import os.path
 import pyautogui
 
 def increase_font():
@@ -5,3 +6,25 @@ def increase_font():
 
 def decrease_font():
     pyautogui.hotkey('command', '-')
+
+def page_up():
+    pyautogui.scroll(20)
+
+def page_down():
+    pyautogui.scroll(-20)
+
+def screenshot():
+    if os.path.isfile('screenshot.png'):
+        count = 0
+        while True:
+            fname = 'screenshot_%i.png' % (count)
+            if not os.path.isfile('screenshot_%i.png' % (count)):
+                pyautogui.screenshot(fname)
+                break
+            count += 1
+    else:
+        pyautogui.screenshot('screenshot.png')
+
+if __name__ == '__main__':
+    pyautogui.click()
+    screenshot()
