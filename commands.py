2d7e2a41e852ef7ef7e24f437fbecbbd2ffc446c
Thomas Huang <thomaseh@umich.edu>
2019-02-08 20:10:19 -0500

    Added mouse click command. Enable chaining of commands with the pipe character.

--- commands.py
+++ commands.py
@@ -5,7 +5,25 @@ import pyautogui
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--command', type=str,
-                    help='command to run')
+                    help='Commands to run.')
+
+"""
+Commands:
+    mc  - mouse click
+    if  - increase font
+    df  - decrease font
+    pu  - page up
+    pd  - page down
+    ss  - screenshot
+
+You can chain together commands by separating them with the '|' character.
+Commands will be executed in the order they are written.
+For example, 'mc | df' will click the mouse then decrease the font size.
+"""
+
+# Commmand functions
+def mouse_click():
+    pyautogui.click()
 
 def increase_font():
     pyautogui.hotkey('command', '+')
@@ -31,15 +49,27 @@ def screenshot():
     else:
         pyautogui.screenshot('screenshot.png')
 
+
+# Helper functions
+def parse_commands(commands):
+    commands = commands.split('|')
+    commands = [command.strip() for command in commands]
+    return commands
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    if args.command == 'increase_font':
-        increase_font()
-    elif args.command == 'decrease_font':
-        decrease_font()
-    elif args.command == 'page_up':
-        page_up()
-    elif args.command == 'page_down':
-        page_down()
-    elif args.command == 'screenshot':
-        screenshot()
+    commands = parse_commands(args.command)
+    for command in commands:
+        if command == 'mc':
+            mouse_click()
+        elif command == 'if':
+            increase_font()
+        elif command == 'df':
+            decrease_font()
+        elif command == 'pu':
+            page_up()
+        elif command == 'pd':
+            page_down()
+        elif command == 'ss':
+            screenshot()
